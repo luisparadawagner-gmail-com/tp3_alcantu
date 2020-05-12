1a54deeda2e25ef40ca7f3950019162c4996b6b2
Gabriela Alcantu <gaby.alcantu.90@live.com.ar>
2020-05-12 13:40:17 -0300

    Se agrega formulario

--- src/app/banda/banda.component.ts
+++ src/app/banda/banda.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-banda',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BandaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  bandaForm = this.fb.group({
+    nombre: ['Rolling Stones', Validators.required],
+    origen: [''],
+    estado: [false],
+    genero: [''],
+    miembros: this.fb.array([ this.fb.control('') ]),
+    fecha: [''],
+    discografica: ['']
+  });
+
+  get miembros(){
+    return this.bandaForm.get('miembros') as FormArray;
+  }
+
+  agregarMiembro(){
+    this.miembros.push(this.fb.control(''));
+  }
+
+  submit(){
+    debugger;
+    this.bandaForm.value;
+
+    this.bandaForm.setValue({
+      nombre: 'Queen',
+      origen: 'Inglaterra',
+      estado: true,
+      genero: 'Rock',
+      miembros: this.bandaForm.get('miembros').value,
+      fecha: this.bandaForm.get('fecha').value,
+      discografica: 'EMI'
+    });
+    this.bandaForm.patchValue({ discografica: 'Capitol Records'});
   }
 
 }
